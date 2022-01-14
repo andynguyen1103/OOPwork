49f33c8d480091b7a99619c64f4dd588525f8337
Andy Nguyen <andynguyenvn1103@gmail.com>
2022-01-14 20:42:14 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GCD_BaitapOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
