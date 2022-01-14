cff4121b5b609a58a3e686f239e912758415b17c
Andy Nguyen <andynguyenvn1103@gmail.com>
2022-01-14 21:28:07 +0700

    second commit

--- Member.cs
+++ Member.cs
@@ -8,5 +8,31 @@ namespace GCD_BaitapOOP
 {
     class Member
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int PhoneNum { get; set; }
+        public string Email { get; set; }
+
+        public Member()
+        {
+            Name = "Unknown";
+            Age = 0;
+            PhoneNum = 0;
+            Email = "Unknown";
+        }
+        public Member(string name, int age, int phoneNum, string email)
+        {
+            Name = name;
+            Age = age>0?age:0;
+            PhoneNum = phoneNum;
+            Email = email;
+        }
+        public virtual void GetBasicInfo()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Age: " + Age);
+            Console.WriteLine("Phone Number: " + PhoneNum);
+            Console.WriteLine("Email: " + Email);
+        }
     }
 }
